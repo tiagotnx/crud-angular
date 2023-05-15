6cee683e714766a659f9b9c2712dae3dc49807be
TiagoTNX <tiagotnx@gmail.com>
2023-05-15 17:59:20 -0300

    Creation of Material Table to List Courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'Front-end' }];
+  displayedColumns = ['name', 'category'];
 }
