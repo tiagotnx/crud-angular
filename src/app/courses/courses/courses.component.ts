96106466301e0aaf5527266da9b66cd65b274467
TiagoTNX <tiagotnx@gmail.com>
2023-05-15 19:15:26 -0300

    Service creation

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 
 import { Course } from '../model/course';
@@ -8,6 +9,10 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'Front-end' }];
+  courses: Course[] = [];
   displayedColumns: string[] = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = coursesService.list();
+  }
 }
