4b3e27043edd5a45a23e2c55035753a4fe45491b
TiagoTNX <tiagotnx@gmail.com>
2023-05-16 00:14:48 -0300

    Course List: Pipe to show icon

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipe/category.pipe';
 
 @NgModule({
   imports: [CommonModule, AppMaterialModule],
-  declarations: [ErrorDialogComponent],
-  exports: [ErrorDialogComponent],
+  declarations: [ErrorDialogComponent, CategoryPipe],
+  exports: [ErrorDialogComponent, CategoryPipe],
 })
 export class SharedModule {}
